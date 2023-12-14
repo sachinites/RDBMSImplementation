53167d39bd05dbe8369ad4771a6ecd1243f418ce
Abhishek Sagar <sachinites@gmail.com>
2023-12-14 13:06:37 +0000

    BPlusTree Library ready to use. Section Completed.

--- Course/SqlParser/compile.sh
+++ Course/SqlParser/compile.sh
@@ -1,18 +1,25 @@
 rm -f *.o
+rm -f ../core/*.o
+rm -f ../BPlusTreeLib/*.o 
+rm -f ../BPlusTreeLib/*.exe 
+rm -f *.exe
 
 lex Parser.l
 g++ -g -c lex.yy.c -o lex.yy.o
 g++ -g -c  SqlParserMain.c -o SqlParserMain.o
 g++ -g -c  SqlCreateParserCFG.c -o SqlCreateParserCFG.o
 g++ -g -c  -fpermissive ../BPlusTreeLib/BPlusTree.c -o ../BPlusTreeLib/BPlusTree.o
+g++ -g -c  -fpermissive ../BPlusTreeLib/main.c -o ../BPlusTreeLib/main.o
+g++ -g ../BPlusTreeLib/BPlusTree.o ../BPlusTreeLib/main.o -o ../BPlusTreeLib/main.exe
 
 #core dir
 g++ -g -c ../core/sql_create.c -o ../core/sql_create.o
-
+g++ -g -c  -fpermissive ../core/BPlusTreeCompFn.c -o ../core/BPlusTreeCompFn.o
 
 g++ -g lex.yy.o \
             SqlParserMain.o \
             SqlCreateParserCFG.o \
             ../core/sql_create.o \
             ../BPlusTreeLib/BPlusTree.o \
+            ../core/BPlusTreeCompFn.o \
             -o dbms.exe -lfl
---
+++ Course/core/BPlusTreeCompFn.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <assert.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../SqlParser/SqlEnums.h"
+#include "../BPlusTreeLib/BPlusTree.h"
+
+// B+ would going to support these three data types as of now = SQL Tables
+// primary key of SQL table = combination of one or more colmns of SQL table = Key of a B+ tree  
+// Key of a B+ Tree = <string 32> <int 4 B> <int 4B> = SQL Table in which the primary key is <name of employee> <emp-id> <dept-id>
+
+// -1 -  key1 > key2 
+//  0  - key1 = key2
+// 1  - key2 > key 1
+static int 
+rdbms_key_comp_fn ( BPluskey_t *key1, BPluskey_t *key2, key_mdata_t *key_mdata, int key_mdata_size) {
+
+    int rc;
+    int dsize;
+    int offset = 0;
+    sql_dtype_t dtype;
+
+    int i;
+
+    if (!key1 || !key1->key || !key1->key_size) return 1;
+    if (!key2 || !key2->key || !key2->key_size) return -1;
+
+    char *key1_ptr = (char *)key1->key;
+    char *key2_ptr = (char *)key2->key;
+
+    for (i = 0; i < key_mdata_size; i++) {
+
+        dtype =  (sql_dtype_t)(key_mdata)[i].dtype; // dtype of 0th element in a B+ Tree key
+        dsize = (key_mdata)[i].size;
+
+        switch (dtype) {
+
+            case SQL_STRING:
+                rc = strncmp (key1_ptr + offset, key2_ptr + offset, dsize) ;
+                if (rc < 0) return 1;
+                if (rc > 0) return -1;
+                offset += dsize;
+                break;
+
+            case SQL_INT:
+                {
+                    int *n1 = (int *)(key1_ptr + offset);
+                    int *n2 = (int *)(key2_ptr + offset);
+                    if (*n1 < *n2) return 1;
+                    if (*n1 > *n2) return -1;
+                    offset += dsize;
+                }
+                break;
+
+            case SQL_DOUBLE:
+              {
+                    double *n1 = (double *)(key1_ptr + offset);
+                    double*n2 = (double *)(key2_ptr + offset);
+                    if (*n1 < *n2) return 1;
+                    if (*n1 > *n2) return -1;
+                    offset += dsize;
+                }
+                break;
+        }
+    }
+
+    return 0;
+}
