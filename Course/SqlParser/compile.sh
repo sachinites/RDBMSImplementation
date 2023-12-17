8fdb835fc0014a0525bbc838a5aa7daa861a7d95
Abhishek Sagar <sachinites@gmail.com>
2023-12-17 12:16:39 +0000

    compile.sh refactoring

--- Course/SqlParser/compile.sh
+++ Course/SqlParser/compile.sh
@@ -4,10 +4,13 @@ rm -f ../BPlusTreeLib/*.o
 rm -f ../BPlusTreeLib/*.exe 
 rm -f *.exe
 
+#SqlParser dir
 lex Parser.l
 g++ -g -c lex.yy.c -o lex.yy.o
 g++ -g -c  SqlParserMain.c -o SqlParserMain.o
 g++ -g -c  SqlCreateParserCFG.c -o SqlCreateParserCFG.o
+
+#B+ Tree Library
 g++ -g -c  -fpermissive ../BPlusTreeLib/BPlusTree.c -o ../BPlusTreeLib/BPlusTree.o
 g++ -g -c  -fpermissive ../BPlusTreeLib/main.c -o ../BPlusTreeLib/main.o
 g++ -g ../BPlusTreeLib/BPlusTree.o ../BPlusTreeLib/main.o -o ../BPlusTreeLib/main.exe
@@ -18,6 +21,7 @@ g++ -g -c  -fpermissive ../core/BPlusTreeCompFn.c -o ../core/BPlusTreeCompFn.o
 g++ -g -c ../core/Catalog.c -o ../core/Catalog.o
 g++ -g -c ../core/sql_delete.c -o ../core/sql_delete.o
 
+#Create an Executable
 g++ -g lex.yy.o \
             SqlParserMain.o \
             SqlCreateParserCFG.o \
