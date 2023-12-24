ce1b7164086ad474bfa6ab2f838a81961a0387eb
Abhishek Sagar <sachinites@gmail.com>
2023-12-24 08:08:10 +0000

    EnumMapper Added.

--- Course/SqlParser/SqlToMexprEnumMapper.c
+++ Course/SqlParser/SqlToMexprEnumMapper.c
@@ -1,10 +1,6 @@
-#include <assert.h>
-#include "ParserExport.h"
+#include <stdint.h>
 #include "SqlEnums.h"
-#include "../../MathExpressionParser/MExprcppEnums.h"
-
-int 
-Appln_to_Mexpr_enum_converter (int token_code);
+#include "../../../MathExpressionParser/Course/MexprcppEnums.h"
 
 int 
 Appln_to_Mexpr_enum_converter (int token_code) {
@@ -39,11 +35,6 @@ Appln_to_Mexpr_enum_converter (int token_code) {
         case SQL_GREATER_THAN:
             return MATH_CPP_GREATER_THAN;
 
-        case SQL_IN:
-            return MATH_CPP_IN;
-        case SQL_LIKE:
-            return MATH_CPP_LIKE;
-
             
         case SQL_BRACKET_START:
             return MATH_CPP_BRACKET_START;
@@ -76,23 +67,17 @@ Appln_to_Mexpr_enum_converter (int token_code) {
         case SQL_STRING_VALUE:
             return MATH_CPP_STRING;
 
-        case SQL_IPV4_ADDR_VALUE:
-            return MATH_CPP_IPV4;
-
         case SQL_IDENTIFIER:
         case SQL_IDENTIFIER_IDENTIFIER:
             return MATH_CPP_VARIABLE;
-
-        case SQL_INTERVAL_VALUE:
-             return MATH_CPP_INTERVAL;
 
         /* Extras */
         case SQL_COMMA:
              return MATH_CPP_COMMA;
 
         default:
-            return PARSER_INVALID_CODE;
+            return -1;
     }
 
-    return PARSER_INVALID_CODE;
+    return -1;
 }
